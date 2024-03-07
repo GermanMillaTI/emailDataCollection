5d445d4bd88dd82242590e643bf28c0d2f99a4a4
German Alejandro Milla Ortiz <german.milla01@telusinternational.com>
2024-03-06 20:01:06 -0600

    add an unnecessary import in the App.js component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import AllPagesPDFViewer from "./components/pdf/all-pages";
 /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
-import samplePDF from "./test3.pdf";
 
 
 export default function App() {
